42c486f00f4eade60f43a946045789e73ca14487
Miguel <miguelmail@gmail.com>
2014-08-18 17:39:57 +0100

    Move help function

--- src/gokyll/gokyll/gokyll.go
+++ src/gokyll/gokyll/gokyll.go
@@ -4,22 +4,26 @@ import "os"
 import "gokyll/files"
 import "fmt"
 
+func help() {
+	fmt.Println(`usage: gokyll directory`)
+}
+
 func main() {
     if len(os.Args) < 2 {
-        files.Help()
+        help()
         return;
     }
     siteDir := os.Args[1]
 	files.MakeSiteDir(siteDir)
-    htmlFiles := files.GetHtmlFilesInDir(siteDir)
-    for _, html := range htmlFiles {
-		files.ProcessFile(siteDir, html)
-    }
     dirs := files.GetSiteDirs(siteDir)
 	for _, dir := range dirs {
 		fmt.Println("Copying: ", dir)
 		files.CopyDirectoryToSite(dir, siteDir)
 	}
+    htmlFiles := files.GetHtmlFilesInDir(siteDir)
+    for _, html := range htmlFiles {
+		files.ProcessFile(siteDir, html)
+    }
 }
 
 
